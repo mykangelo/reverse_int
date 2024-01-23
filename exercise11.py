c35412dcc1ad368a52c30151cde15bd7f3661a23
mykangelo <mmykangelo@gmail.com>
2024-01-23 17:32:04 +0800

    Answer for exercise number 11

---
+++ exercise11.py
@@ -0,0 +1,12 @@
+
+
+
+#store the given integers in a variable and convert it to string
+given_int = (str(7536))
+
+#print the given interger
+print("The given integers is:" , given_int)
+#print the reverse order and use the join method to returns a string created by joining the elements of an iterable by string separator then use the reversed method.
+print("Number in reverse order: " , ' ' .join(reversed(given_int)))
+
+
